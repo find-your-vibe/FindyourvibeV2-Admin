633544ef4d9cc7a4941bd24187eb3975995ef787
gamandeepsingh <gamandeepsingh4@gmail.com>
2025-04-10 12:57:10 +0530

    feat: update API URLs in environment configuration for production

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,18 +1,16 @@
 export const environment = {
     production: false,
     apiUrl:{
-        auth: 'http://localhost:5000',
-        event: 'http://localhost:5005',
-        user: 'http://localhost:5001',
-        upload: 'http://localhost:5003',
-        location: 'http://localhost:5007',
-        content: 'http://localhost:5010',
-        offer: 'http://localhost:5006',
-        onlinePayment: 'http://localhost:5008',
-        email: 'http://localhost:5009',
+        auth: 'https://api.auth.findyourvibe.in',
+        event: 'https://api.event.findyourvibe.in',
+        user: 'https://api.user.findyourvibe.in',
+        upload: 'https://api.event.findyourvibe.in',
+        location: 'https://api.location.findyourvibe.in',
+        content: 'https://api.content.findyourvibe.in',
+        offer: 'https://api.offers.findyourvibe.in',
+        onlinePayment: 'https://api.online.findyourvibe.in',
+        email: 'https://api.email.findyourvibe.in',
     },
     googleClientId:'473749765121-0h0b6ue1t8bpqu7d4lldjg6ju94c05sd.apps.googleusercontent.com',
     googleMapsApiKey: 'AIzaSyC0lJdNBz8rqy4cPNu7u-WRYDXZwepMYp8',
-    recaptchaSiteKey: '6LcS-vwqAAAAAE2-W985Ry49M3TkTOEqwWQjZtrO',
-    razorPayKey: 'rzp_test_076hrxbWJPCBcc'
 };
