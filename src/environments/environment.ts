360b58cda5d2bf80dc0b19086ece3c0f252d84e2
gamandeepsingh <gamandeepsingh4@gmail.com>
2025-05-06 21:21:05 +0530

    fix: update API URLs in environment configuration for production

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,16 +1,16 @@
 export const environment = {
     production: false,
-    apiUrl: {
+    apiUrl:{
         auth: 'https://api.auth.findyourvibe.in',
-        event: 'http://localhost:5005',
+        event: 'https://api.event.findyourvibe.in',
         user: 'https://api.user.findyourvibe.in',
-        upload: 'http://localhost:5005',
+        upload: 'https://api.event.findyourvibe.in',
         location: 'https://api.location.findyourvibe.in',
         content: 'https://api.content.findyourvibe.in',
         offer: 'https://api.offers.findyourvibe.in',
-        onlinePayment: 'http://localhost:5008',
+        onlinePayment: 'https://api.online.findyourvibe.in',
         email: 'https://api.email.findyourvibe.in',
-      },
+    },
     googleClientId:'473749765121-0h0b6ue1t8bpqu7d4lldjg6ju94c05sd.apps.googleusercontent.com',
     googleMapsApiKey: 'AIzaSyC0lJdNBz8rqy4cPNu7u-WRYDXZwepMYp8',
 };
