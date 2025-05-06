b21c1e63ed82864843ac9c64bdc541700469ec4b
gamandeepsingh <gamandeepsingh4@gmail.com>
2025-05-06 21:18:46 +0530

    fix: improve accessibility and user guidance in edit event component

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,16 +1,16 @@
 export const environment = {
     production: false,
-    apiUrl:{
+    apiUrl: {
         auth: 'https://api.auth.findyourvibe.in',
-        event: 'https://api.event.findyourvibe.in',
+        event: 'http://localhost:5005',
         user: 'https://api.user.findyourvibe.in',
-        upload: 'https://api.event.findyourvibe.in',
+        upload: 'http://localhost:5005',
         location: 'https://api.location.findyourvibe.in',
         content: 'https://api.content.findyourvibe.in',
         offer: 'https://api.offers.findyourvibe.in',
-        onlinePayment: 'https://api.online.findyourvibe.in',
+        onlinePayment: 'http://localhost:5008',
         email: 'https://api.email.findyourvibe.in',
-    },
+      },
     googleClientId:'473749765121-0h0b6ue1t8bpqu7d4lldjg6ju94c05sd.apps.googleusercontent.com',
     googleMapsApiKey: 'AIzaSyC0lJdNBz8rqy4cPNu7u-WRYDXZwepMYp8',
 };
